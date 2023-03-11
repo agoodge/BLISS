896f987e0c6482078f8cb25549c81852a50ed2df
agoodge <adamgoodge@hotmail.com>
2023-03-11 11:59:27 +0800

    11/03/2023 init

---
+++ data_utils.py
@@ -0,0 +1,178 @@
+"""
+Code submission for CAD-CLIP: Calibrated Anomaly Detection with Contrastive Language-Image Pre-training.
+
+Some of the code has been adapted from https://github.com/sesmae/ZOC
+
+"""
+
+
+
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+import os
+import torch
+import glob
+from torchvision.datasets import CIFAR10, CIFAR100
+from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize, ToPILImage
+from PIL import Image
+from torchvision.datasets import ImageFolder
+
+class cifar10(Dataset):
+    def __init__(self, train):
+        super(cifar10, self).__init__()
+        self.transform = Compose([
+            ToPILImage(),
+            Resize(224, interpolation=Image.BICUBIC),
+            CenterCrop(224),
+            ToTensor(),
+            Normalize((0.4913, 0.4821, 0.4465), (0.2470, 0.2434, 0.2615))
+        ])
+
+        if train:
+            ds = CIFAR10(root='../data', train=True, download=True)
+        else:
+            ds = CIFAR10(root='../data', train=False, download=True)
+
+        self.data = ds.data
+        self.targets = ds.targets
+        self.classes = ds.classes
+        self.class_to_idx = ds.class_to_idx
+        self.zoc_splits =  [[0, 1, 9, 7, 3, 2],
+                            [0, 2, 4, 3, 7, 5],
+                            [5, 1, 9, 8, 7, 0],
+                            [5, 7, 1, 8, 4, 6],
+                            [8, 1, 5, 3, 4, 6]
+                           ]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.transform(self.data[index])
+
+class cifar100(Dataset):
+    def __init__(self, train):
+        super(cifar100, self).__init__()
+        coarse_labels = np.array([ 4,  1, 14,  8,  0,  6,  7,  7, 18,  3,  
+                               3, 14,  9, 18,  7, 11,  3,  9,  7, 11,
+                               6, 11,  5, 10,  7,  6, 13, 15,  3, 15,  
+                               0, 11,  1, 10, 12, 14, 16,  9, 11,  5, 
+                               5, 19,  8,  8, 15, 13, 14, 17, 18, 10, 
+                               16, 4, 17,  4,  2,  0, 17,  4, 18, 17, 
+                               10, 3,  2, 12, 12, 16, 12,  1,  9, 19,  
+                               2, 10,  0,  1, 16, 12,  9, 13, 15, 13, 
+                              16, 19,  2,  4,  6, 19,  5,  5,  8, 19, 
+                              18,  1,  2, 15,  6,  0, 17,  8, 14, 13])
+    
+        self.transform = Compose([
+            ToPILImage(),
+            Resize(224, interpolation=Image.BICUBIC),
+            CenterCrop(224),
+            ToTensor(),
+            Normalize((0.4913, 0.4821, 0.4465), (0.2470, 0.2434, 0.2615))
+        ])
+
+        if train:
+            ds = CIFAR100(root='../data', train=True, download=True)
+        else:
+            ds = CIFAR100(root='../data', train=False, download=True)
+        
+        self.data = ds.data
+        self.targets = ds.targets
+        self.classes = ds.classes
+        self.class_to_idx = ds.class_to_idx
+        self.zoc_splits = [list(range(20)), list(range(20, 40)), list(range(40, 60)), list(range(60, 80)), list(range(80, 100))]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.transform(self.data[index])
+
+def data_loader(dataset, train):
+    if dataset == 'cifar10':
+        dataset = cifar10(train)
+    elif dataset == 'cifar100':
+        dataset = cifar100(train)
+
+    loader = DataLoader(dataset=dataset, batch_size=128, num_workers=4)
+    return loader
+
+def tinyimage_semantic_split_generator():
+   index_splits = [
+       [192, 112, 145, 107, 91, 180, 144, 193, 10, 125, 186, 28, 72, 124, 54, 77, 157, 169, 104, 166],
+       [156, 157, 167, 175, 153, 11, 147, 0, 199, 171, 132, 60, 87, 190, 101, 111, 193, 71, 131, 192],
+       [28, 15, 103, 33, 90, 167, 61, 13, 124, 159, 49, 12, 54, 78, 82, 107, 80, 25, 140, 46],
+       [128, 132, 123, 72, 154, 35, 86, 10, 188, 28, 85, 89, 91, 82, 116, 65, 96, 41, 134, 25],
+       [102, 79, 47, 106, 59, 93, 145, 10, 62, 175, 76, 183, 48, 130, 38, 186, 44, 8, 29, 26]]  # CAC splits
+   dataset = ImageFolder(root='../data/tiny-imagenet-200/val')
+   a=dataset.class_to_idx
+   reverse_a = {v:k for k,v in a.items()}
+   semantic_splits = [[],[],[],[],[]]
+   for i, split in enumerate(index_splits):
+       wnid_split = []
+       for idx in split:
+           wnid_split.append(reverse_a[idx])
+       all = list(dataset.class_to_idx.keys())
+       seen = wnid_split
+       unseen = list(set(all)-set(seen))
+       #seen.extend(unseen)
+       f = open('./imagenet_utils/imagenet_id_to_label.txt', 'r')
+       imagenet_id_idx_semantic = f.readlines()
+
+       for id in seen:
+           for line in imagenet_id_idx_semantic:
+               if id == line[:-1].split(' ')[0]:
+                   semantic_label = line[:-1].split(' ')[2]
+                   semantic_label = semantic_label.replace("_", " ")
+                   semantic_splits[i].append(semantic_label)
+                   break
+   return index_splits, semantic_splits
+
+class tinyimagenet(Dataset):
+    def __init__(self, train, mappings_dict):
+        super(tinyimagenet, self).__init__()
+        self.index_splits, self.semantic_splits = tinyimage_semantic_split_generator()
+        self.image_paths = []
+        self.targets = []
+
+        # run imagenet_utils/tinyimagenet.sh to download tinyimagenet data
+        if train == True:
+            path = '../data/tiny-imagenet-200/train/'
+        else:
+            path = '../data/tiny-imagenet-200/val/'
+        for label in mappings_dict.keys():
+            temp_path = path + f"{mappings_dict[label]}"
+            self.image_paths.extend(glob.glob(os.path.join(temp_path,'*.JPEG')))
+            self.targets.extend(len(glob.glob(os.path.join(temp_path,'*.JPEG')))*[label])
+        self.transform = Compose([
+            Resize(224, interpolation=Image.BICUBIC),
+            CenterCrop(224),
+            ToTensor(),
+            Normalize((0.4913, 0.4821, 0.4465), (0.2470, 0.2434, 0.2615))
+        ])
+        self.classes = list(mappings_dict.keys())
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, index):
+        x = Image.open(self.image_paths[index]).convert('RGB')
+        if self.transform:
+            x = self.transform(x)
+        return x
+
+def tinyimage_loader(train):
+    
+    f = open('./imagenet_utils/tinyimagenet_labels_to_ids.txt', 'r')
+    #f = open('../tinyimagenet_ids_to_label.txt', 'r')
+    tinyimg_label2folder = f.readlines()
+    mappings_dict = {}
+    for line in tinyimg_label2folder:
+        label, class_id = line[:-1].split(' ')[0], line[:-1].split(' ')[1]
+        label = label.replace("_", " ")
+        mappings_dict[label] = class_id
+
+    dataset = tinyimagenet(train, mappings_dict)
+    loader = DataLoader(dataset=dataset, batch_size=128, num_workers=4)
+    return loader
---
+++ imagenet_eval.py
@@ -0,0 +1,157 @@
+"""
+Code submission for CAD-CLIP: Calibrated Anomaly Detection with Contrastive Language-Image Pre-training.
+
+Some of the code has been adapted from https://github.com/sesmae/ZOC
+
+"""
+
+import os
+import torch
+import data_utils
+import faiss
+import argparse
+from tqdm import tqdm
+import argparse
+import numpy as np
+from sklearn.metrics import roc_auc_score
+from imagenet_utils.dictionary import dictionary
+from clip.simple_tokenizer import SimpleTokenizer as clip_tokenizer
+import warnings
+warnings.filterwarnings("ignore")
+
+# tokenize sentences
+def tokenize_for_clip(batch_sentences, tokenizer):
+    default_length = 77
+    sot_token = tokenizer.encoder['<|startoftext|>']
+    eot_token = tokenizer.encoder['<|endoftext|>']
+    tokenized_list = []
+    for sentence in batch_sentences:
+        text_tokens = [sot_token] + tokenizer.encode(sentence) + [eot_token]
+        tokenized = torch.zeros((default_length), dtype=torch.long)
+        tokenized[:len(text_tokens)] = torch.tensor(text_tokens)
+        tokenized_list. append(tokenized)
+    tokenized_list = torch.stack(tokenized_list)
+    return tokenized_list
+
+# encode images from loader with clip model
+def image_encoder(clip_model, device, loader):
+    with torch.no_grad():
+        image_features = []
+        for idx, image in enumerate(tqdm(loader)):
+            image_feature = clip_model.encode_image(image.to(device)).float()
+            image_features.append(image_feature.detach().cpu().numpy())
+        image_features = np.vstack(image_features)
+    return image_features
+
+# encode text in labels with clip_model
+def text_encoder(clip_model, device, labels):
+    with torch.no_grad():
+        cliptokenizer = clip_tokenizer()
+        seen_descriptions = [f"This is a photo of a {label}" for label in labels]
+        seen_descriptions_tokens = tokenize_for_clip(seen_descriptions, cliptokenizer)
+        text_features = clip_model.encode_text(seen_descriptions_tokens.to(device)).float()
+    return text_features.detach().cpu().numpy()
+
+# return cosine similarity between normalized features
+def sim(image_features, text_features):
+    return image_features @ text_features.T
+
+# similarity to normal text labels from dataset
+def internal_class_score(normal_val_image_features, test_image_features, dataset_text_features):
+    val_dataset_text_distances = sim(normal_val_image_features,dataset_text_features)                
+    dataset_text_mu = np.mean(val_dataset_text_distances,axis=0)
+    dataset_text_std = np.std(val_dataset_text_distances,axis=0) + 1e-8
+    test_dataset_text_distances = sim(test_image_features,dataset_text_features)
+    score = -(test_dataset_text_distances - dataset_text_mu)/dataset_text_std
+    return score.squeeze()
+
+# similarity to text labels from dictionary
+def external_test_score(normal_val_image_features, test_image_features, dictionary_text_features, topk_dictionary_distances, topk_dictionary_labels):
+    val_dictionary_text_distances = sim(normal_val_image_features,dictionary_text_features)
+    dictionary_text_mu = np.mean(val_dictionary_text_distances, axis = 0)
+    dictionary_text_std = np.std(val_dictionary_text_distances, axis = 0) + 1e-8
+    dictionary_score = (topk_dictionary_distances - dictionary_text_mu[topk_dictionary_labels])/dictionary_text_std[topk_dictionary_labels]
+    dictionary_score = dictionary_score.reshape(len(dictionary_score),-1)
+    score = dictionary_score.mean(axis=1)
+    return score
+
+# normalize features to unit sphere
+def norm(features):
+    features /= np.linalg.norm(features,axis=1)[:, None]
+    return features
+
+def main(args):
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # download the model from "https://openaipublic.azureedge.net/clip/models/5806e77cd80f8b59890b7e101eabd078d9fb84e6937f9e85e4ecb61988df416f/ViT-B-16.pt"
+    clip_model = torch.jit.load(os.path.join('./trained_models', "{}.pt".format('ViT-B-16')))#.to(device).eval()
+
+    print("Encoding training images...")
+    val_loader = data_utils.tinyimage_loader(train = True)
+    val_image_features = image_encoder(clip_model, device, val_loader)
+    val_image_features = norm(val_image_features)
+    val_image_targets = np.array(val_loader.dataset.targets)
+    
+    print("Encoding test images...")
+    test_loader = data_utils.tinyimage_loader(train = False)
+    test_image_features = image_encoder(clip_model, device, test_loader)
+    test_image_features = norm(test_image_features)
+    test_image_targets = np.array(test_loader.dataset.targets)
+
+    print("Encoding text...")
+    global dictionary
+    dictionary_list = list(dictionary.values())
+    dictionary = []
+    for label in tqdm(range(len(dictionary_list))):
+        for item in dictionary_list[label].split(','):
+            dictionary.append(item)
+    #dictionary = list(set(dictionary)-set(test_loader.dataset.targets))
+    dictionary_text_features = text_encoder(clip_model, device, dictionary)
+    dictionary_text_features = norm(dictionary_text_features)
+
+    dataset_text_features = text_encoder(clip_model, device, val_loader.dataset.classes)
+    dataset_text_features = norm(dataset_text_features)
+
+    # find the topk most similar words in dictionary_text_features to each image in test_image_features
+    test_dictionary_text_distances = sim(test_image_features, dictionary_text_features)
+    topk_dictionary_distances = np.sort(test_dictionary_text_distances,axis=1)[:,-args.topk:].squeeze()
+    topk_dictionary_labels = np.argsort(test_dictionary_text_distances,axis=1)[:,-args.topk:].squeeze()
+
+    split_auc_list = []
+
+    for normal_target_set in val_loader.dataset.semantic_splits:
+        all_class_scores = []
+        # for each class in the set of normal classes
+        for single_class in normal_target_set:
+            
+            #only normal images belonging to digit class
+            normal_val_image_features = val_image_features[np.where(np.array(val_loader.dataset.targets) == single_class)]
+
+            single_class_score = np.zeros(len(test_image_features))
+
+            # similarity to normal text labels from datasets)
+            i_scores = internal_class_score(normal_val_image_features, test_image_features, dataset_text_features[np.where(np.array(val_loader.dataset.classes) == single_class)])
+            single_class_score += i_scores
+
+            # similarity to text labels from dictionary
+            e_scores = external_test_score(normal_val_image_features, test_image_features, dictionary_text_features, topk_dictionary_distances, topk_dictionary_labels)
+            single_class_score += args.lam*e_scores
+            
+            all_class_scores.append(single_class_score)
+        scores = np.array(all_class_scores).min(axis=0)
+        test_normal_targets = np.array([0 if i in normal_target_set else 1 for i in test_loader.dataset.targets])
+        auc = 100*roc_auc_score(test_normal_targets, scores)
+        split_auc_list.append(auc)
+
+    split_auc_list = np.array(split_auc_list)
+    print("Mean AUROC:",np.round(np.mean(split_auc_list,axis=0),1))
+    print("Std Deviation:", np.round(np.std(split_auc_list,axis=0),1))
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--topk", type = str, default = 10, help = 'Number of dictionary words to use in external text score')
+    parser.add_argument("--lam", type = float, default = 0.5, help = 'Value of the hyper-parameter weight of external text score')
+    args = parser.parse_args()
+
+    main(args)
